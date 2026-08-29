c931ab650f100442ae8b91727fb53c9fafc6a0a2
rishana <rishhana5@gmail.com>
2026-08-29 11:08:28 +0530

    production error

--- sampleproject/urls.py
+++ sampleproject/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ("",include('sampleApp.urls')),
-    path ("APP1/",include('APP1.urls'))
+    path ("app1/",include('APP1.urls'))
     
 ]
 
